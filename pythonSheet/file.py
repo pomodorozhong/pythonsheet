5053b378c363e9a8fcf35e6dd66e9be4bba6cb88
pomodorozhong <pomodorozhong@gmail.com>
2020-07-21 17:06:21 +0800

    feat✨(file.py): add replace_a_char_each_line()

--- pythonSheet/file.py
+++ pythonSheet/file.py
@@ -4,3 +4,18 @@ def line_slice(in_path, out_path, start: int, end: int):
             for i, line in enumerate(input):
                 if i >= start and i < end:
                     out.write(line)
+
+
+def replace_a_char_each_line(file_in, file_out, char_index: int, replace_with):
+    with open(file_in) as fp:
+        lines_to_be_replace = fp.read().splitlines()
+
+    with open(file_out, "w") as fp:
+        for line in lines_to_be_replace:
+            if len(line) > char_index:
+                char_list = list(line)
+                char_list[char_index] = replace_with
+                modifid_line = ''.join(char_list)
+                print(modifid_line, file=fp)
+            else:
+                print(line, file=fp)
