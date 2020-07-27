b7a3f9db3c3ab7784577adea658164eed3d92aa9
pomodorozhong <pomodorozhong@gmail.com>
2020-07-27 20:15:04 +0800

    feat✨(utility): add fit()

--- pythonSheet/utility.py
+++ pythonSheet/utility.py
@@ -1,5 +1,7 @@
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
+from scipy import stats
 
 
 def match(search_keys: 'series', search_range: 'series'):
@@ -31,3 +33,36 @@ def index(references: 'series', rows_to_extract: 'series'):
 def combine_series_to_dataframe(series_array):
     dataframe = pd.concat(series_array, axis=1)
     return dataframe
+
+
+def fit(x: 'series', y: 'series', deg: int, plot_file=""):
+
+    df = combine_series_to_dataframe([x, y])
+
+    # remove rows that contains non-numeric
+    df = df[pd.to_numeric(df[x.name], errors='coerce').notnull()]
+    df = df[pd.to_numeric(df[y.name], errors='coerce').notnull()]
+    df[x.name] = pd.to_numeric(df[x.name])
+    df[y.name] = pd.to_numeric(df[y.name])
+
+    # remove outliers
+    df = df[(np.abs(stats.zscore(df)) < 4).all(axis=1)]
+
+    x = getattr(df, x.name)
+    y = getattr(df, y.name)
+    model = np.polyfit(x, y, deg)
+    predict = np.poly1d(model)
+
+    if plot_file != "":
+        # plotting
+        x_lin_reg = range(0, 51)
+        y_lin_reg = predict(x_lin_reg)
+        plt.clf()
+        plt.scatter(x, y)
+        plt.plot(x_lin_reg, y_lin_reg, c='r')
+        plt.title(predict.c)
+
+        # save plot
+        plt.savefig(plot_file)
+
+    return predict.c
