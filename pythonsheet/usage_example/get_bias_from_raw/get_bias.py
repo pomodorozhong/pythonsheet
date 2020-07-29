32f10fbe790dc861f264cc5b4ee0247f2d25cec5
pomodorozhong <pomodorozhong@gmail.com>
2020-07-29 15:03:27 +0800

    docs📝(usage_example): add get_bias_from_raw/

---
+++ pythonsheet/usage_example/get_bias_from_raw/get_bias.py
@@ -0,0 +1,17 @@
+import pythonsheet.pythonsheet.utility as utility
+import pythonsheet.pythonsheet.major as pm
+import pythonsheet.pythonsheet.file as pfile
+import pythonsheet.pythonsheet.csv_ as csv
+
+
+files_in = ["./pythonsheet/usage_example/get_bias_series_from_raw/raw.csv"]
+files_out = ["./pythonsheet/usage_example/get_bias_series_from_raw/bias.csv"]
+ground_truth = 75
+
+for i in range(len(files_in)):
+    series = csv.get_series(files_in[i], "measurement")
+
+    measurement_bias = pm.add(series, -1 * ground_truth)
+
+    measurement_bias.name = "measurement_bias"
+    csv.write_series_to_csv(files_out[i], measurement_bias)
