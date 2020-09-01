2ba5aa4b16797db9c64ffa1a6eaaef1afca4f006
pomodorozhong <pomodorozhong@gmail.com>
2020-09-02 00:16:21 +0800

    chore⚙️(scripts): extract mkdir out of env_init.sh

--- scripts/mkdir.sh
+++ scripts/mkdir.sh
@@ -8,14 +8,10 @@ parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 cd "$parent_path"
 cd ..
 
-# Make directory for Python venv
-python3 -m venv .venv
-
-# Activate virtual environment
-source .venv/bin/activate
-
-# Install dependencies
-pip3 install -r requirements.txt
+# Make directory to store input/output file.
+mkdir in
+mkdir out
+mkdir out/plot
 
 # Reset working directory
 cd "$origin_wd"
