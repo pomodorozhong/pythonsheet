53e0f082e14c0c04b51eec80bb1285a501d5fccc
pomodorozhong <pomodorozhong@gmail.com>
2020-12-07 19:15:42 +0800

    feat✨(file): add replace_a_char_series_each_line()

--- pythonsheet/file.py
+++ pythonsheet/file.py
@@ -19,3 +19,22 @@ def replace_a_char_each_line(file_in, file_out, char_index: int, replace_with):
                 print(modifid_line, file=fp)
             else:
                 print(line, file=fp)
+
+
+def replace_a_char_series_each_line(file_in, file_out, start_index: int, end_index: int, replace_with):
+    if end_index < start_index:
+        raise Exception("end_index < start_index")
+
+    with open(file_in) as fp:
+        lines_to_be_replace = fp.read().splitlines()
+
+    with open(file_out, "w") as fp:
+        for line in lines_to_be_replace:
+            if len(line) > end_index:
+                char_list = list(line)
+                left = char_list[:start_index]
+                right = char_list[end_index+1:]
+                modifid_line = ''.join(left) + replace_with + ''.join(right)
+                print(modifid_line, file=fp)
+            else:
+                print(line, file=fp)
