3d968ebd875663cdf0c9ba0d1ef278648066b7ea
pomodorozhong <pomodorozhong@gmail.com>
2021-05-02 23:44:42 +0800

    feat✨(plot): add plot.py with some To-Do list

---
+++ pythonsheet/plot.py
@@ -0,0 +1,10 @@
+import pandas as pd
+
+
+def scatter():
+    # To-Do features
+    # 1. options to remove incomplete data row
+    # 2. merge series (to have unified x axis)
+    # 3. auto scale
+    # 4. auto scale without outlier stretching the scale
+    # 5. trend line with coefficient and r2
